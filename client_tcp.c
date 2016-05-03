d4016b10a65da31fa251392c2bedbd7900d9fb3b
Fabio Ferrero <fabio.ferrero@icloud.com>
2016-05-03 11:41:40 +0200

    Added request creation by client. TODO modify conn.hc conn_functions in order to use Connection instead of Host

--- client_tcp.c
+++ client_tcp.c
@@ -4,9 +4,16 @@
 
 #include "conn.h"
 
+#define OK "+OK\r\n"
+#define ERROR "-ERR\r\n"
+#define TOKEN 65536
+#define MAX_FILENAME 51
+
 int main(int argc, char *argv[]) {
 
 	Connection conn;
+	char filename[MAX_FILENAME], * request;
+	int str_len;
 	
 	if (argc != 3) {
 		fprintf(stderr, "syntax: %s <address> <port>\n", argv[0]); 
@@ -17,5 +24,29 @@ int main(int argc, char *argv[]) {
 	
 	conn = conn_connect(argv[1], checkport(argv[2]));
 	
+	printf("Insert filename or <quit> to close the connection\n");
+	
+	while(1) {
+		printf("> ");
+		str_len = readline(filename, MAX_FILENAME);
+		
+		if (!strcmp(filename, "QUIT") || !strcmp(filename, "quit")) {
+			printf("Terminating connection with server...\n");
+			close(conn.id);
+			break;
+		}
+		printf("\trequested file: %s\n", filename);
+		
+		request = malloc((str_len+6)*sizeof(*request));
+		
+		sprintf(request, "GET %s\r\n", filename);
+		
+		//TODO modify connection function using Connection instead Host
+		//conn_sends(, request);
+		
+	}
+	
 	return 0;
 }
+
+
